fd04fe9d898421415bc2be70fbabf41c0eaf2f09
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-10-10 12:17:31 -0300

    build(class): atualiza a build

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -15,12 +15,28 @@ class Cardnews extends HTMLElement{
         const componentRoot = document.createElement("div");
         componentRoot.setAttribute("class", "card");
 
+        //criando a classe 'card_left':
         const cardLeft = document.createElement("div");
         cardLeft.setAttribute("class", "card_left");
 
+        //Criando a 'span', 'a', 'p':
+        const author = document.createElement("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
+        
+        cardLeft.appendChild(author);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        //Criando a classe 'card_right':
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class", "card_right");
 
+        //Criando o 'img' para o 'card_right':
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+
+        //Atribuindo os 'cardLeft' e 'cardRight' como 'child' do 'componentRoot': 
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
 

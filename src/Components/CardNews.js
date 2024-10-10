7b37d5442344c104cca95bd800a0eddb86adf043
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-10-10 11:48:07 -0300

    build(element): cria os elementos HTML

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -5,12 +5,26 @@ class Cardnews extends HTMLElement{
         super()
 
         const shadow = this.attachShadow({mode: "open"});
-
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
 
     }
 
     build(){
+        //Obs.: ao invés de usarmos 'card', o termo 'componentRoot' está sendo declarado, pois é costume ser usado como conceito de primeiro, ou pai, ou principal, aquele que começa algo.
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
 
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card_left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card_right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
     }
     styles(){
 

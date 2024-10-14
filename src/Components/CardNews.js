e5e5344e59952d5cef2aa45037120fb08d838afd
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-10-14 17:20:54 -0300

    build(style): atualiza o style do component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -51,7 +51,57 @@ class Cardnews extends HTMLElement{
         return componentRoot;
     }
     styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+        
+.card {
+    width: 900px;
+    border: 1px solid gray;
+    display: flex;
+    /*coloca a caixa direita e a esquerda em linha horizonal*/
+    flex-direction: row;
+    /* Se colocarm 'column' ao invés de 'row', voltará para vertical */
+    /* cssmatic.com: box-shadow utilizado do site */
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(230, 230, 230, 1);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(230, 230, 230, 1);
+    box-shadow: 10px 10px 5px 0px rgba(230, 230, 230, 1);
+}
 
+
+
+.card_left {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 10px;
+}
+
+.card_left > span {
+    font-weight: 400;
+    margin-right: 40px;
+}
+
+.card_left > a {
+    /* '>' você irá alterar apenas o elemento h1, segundo o exemplo*/
+    margin-top: 15px;
+    /* aumenta o espaçamento entre o título e nome do autor */
+    font-size: 25px;
+    color: black;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.card_left>p {
+    color: gray;
+}
+
+.card_right {
+    margin: 5px;
+    margin-right: 5px;
+}
+
+        `
+        return style;
     }
 }
 
